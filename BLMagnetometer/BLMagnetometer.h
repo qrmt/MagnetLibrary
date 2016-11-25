24e273f634eefba3e2648611b32790c3e71af2e9
Oskar Vuola <ovuola@gmail.com>
2016-11-25 20:49:15 +0200

    Initial Commit

---
+++ BLMagnetometer/BLMagnetometer.h
@@ -0,0 +1,13 @@
+//
+//  BLMagnetometer.h
+//  BLMagnetometer
+//
+//  Created by Oskar Vuola on 25/11/16.
+//  Copyright © 2016 blastly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLMagnetometer : NSObject
+
+@end
---
+++ BLMagnetometer/BLMagnetometer.m
@@ -0,0 +1,13 @@
+//
+//  BLMagnetometer.m
+//  BLMagnetometer
+//
+//  Created by Oskar Vuola on 25/11/16.
+//  Copyright © 2016 blastly. All rights reserved.
+//
+
+#import "BLMagnetometer.h"
+
+@implementation BLMagnetometer
+
+@end
